7a416202ddde88dc7c6cb77def8c0ef33d688a37
Piotr Jamiński <pjaminski@objectivity.co.uk>
2018-04-12 08:46:12 +0200

    Add default and watch tasks for gulp

--- WaitForIt/gulpfile.js
+++ WaitForIt/gulpfile.js
@@ -2,14 +2,25 @@ var gulp = require("gulp"),
     less = require("gulp-less"),
     cleanCSS = require('gulp-clean-css');
 
+gulp.task('default', ['less'], function () {
+    minifyCss();
+});
+
 gulp.task("less", function () {
     return gulp.src('Styles/site.less')
         .pipe(less())
         .pipe(gulp.dest('wwwroot/css'));
 });
 
-gulp.task('minify-css', function () {
+gulp.task('minify-css', () => minifyCss());
+
+gulp.task('watch', ['default'], function () {
+    gulp.watch('Styles/site.less', ['default']);
+});
+
+function minifyCss() {
     return gulp.src('wwwroot/css/*.css')
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(gulp.dest('wwwroot/css'));
-});
+}
+
